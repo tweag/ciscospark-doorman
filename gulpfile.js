4114291b094416191d6691246ecc6afbd8b5c82a
Mike Nicholaides <mike.nicholaides@gmail.com>
2017-02-24 16:43:42 -0500

    Use lambda syntax in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
-const gulp = require('gulp');
-const $    = require('gulp-load-plugins')();
+const gulp = require('gulp')
+const $    = require('gulp-load-plugins')()
 
-const imagesDir = 'src/images';
+const imagesDir = 'src/images'
 
-gulp.task('images', function() {
-  return gulp.src(`${imagesDir}/**/*`)
+gulp.task('images', () =>
+  gulp.src(`${imagesDir}/**/*`)
     .pipe(gulp.dest('public/images'))
-});
+)
 
 const sassPaths = [
   'bower_components/normalize.scss/sass',
   'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
-];
+  'bower_components/motion-ui/src',
+]
 
-const stylesheetsDir = 'src/stylesheets';
+const stylesheetsDir = 'src/stylesheets'
 
-gulp.task('sass', function() {
-  return gulp.src(`${stylesheetsDir}/index.s*ss`)
+gulp.task('sass', () =>
+  gulp.src(`${stylesheetsDir}/index.s*ss`)
     .pipe(
       $.sass({
         includePaths: sassPaths,
@@ -27,27 +27,27 @@ gulp.task('sass', function() {
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest('public/stylesheets'));
-});
+    .pipe(gulp.dest('public/stylesheets'))
+)
 
-gulp.task('watch-sass', ['sass'], function() {
-  return gulp.watch([`${stylesheetsDir}/**/*.scss`], ['sass']);
-});
+gulp.task('watch-sass', ['sass'], () =>
+  gulp.watch([`${stylesheetsDir}/**/*.scss`], ['sass'])
+)
 
 
-function cleanTask(name, dir) {
-  gulp.task(`clean-${name}`, function() {
-    return gulp.src(dir, {read: false})
-      .pipe($.clean());
-  });
+function cleanTask (name, dir) {
+  gulp.task(`clean-${name}`, () =>
+    gulp.src(dir, {read: false})
+      .pipe($.clean())
+  )
 }
 
-cleanTask('sass', 'public/stylesheets/*');
-cleanTask('images', 'public/images/*');
-cleanTask('transpiled', 'lib/*');
+cleanTask('sass', 'public/stylesheets/*')
+cleanTask('images', 'public/images/*')
+cleanTask('transpiled', 'lib/*')
 
 gulp.task('clean', ['clean-sass', 'clean-images', 'clean-transpiled'])
 
-gulp.task('build', ['images', 'sass']);
+gulp.task('build', ['images', 'sass'])
 
-gulp.task('default', ['build', 'watch-sass']);
+gulp.task('default', ['build', 'watch-sass'])
